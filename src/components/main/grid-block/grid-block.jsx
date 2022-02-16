f2478d08da188b1e5ec5390674393a004dc4e087
Gaurrus <listenbulletformyvalentine@gmail.com>
2022-02-16 20:32:30 +0300

    clothes исправил отображенеие карточек

--- src/components/main/grid-block/grid-block.jsx
+++ src/components/main/grid-block/grid-block.jsx
@@ -11,26 +11,44 @@ export const GridBlock = ({ dresses, productType, particular }) => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.gridBlock}>
-        {dresses
-          .filter((item) => item?.particulars[particular])
-          .filter((_, index) => index <= 7)
-          .map((item) => (
-            <NavLink
-              key={item.id}
-              to={`/${productType}/${item.id}`}
-              className={styles.link}
-              data-test-id={`clothes-card-${productType}`}
-            >
-              <GridItem
+        {particular
+          ? dresses
+              .filter((item) => item?.particulars[particular])
+              .filter((_, index) => index <= 7)
+              .map((item) => (
+                <NavLink
+                  key={item.id}
+                  to={`/${productType}/${item.id}`}
+                  className={styles.link}
+                  data-test-id={`clothes-card-${productType}`}
+                >
+                  <GridItem
+                    key={item.id}
+                    img={item.images[0].url}
+                    title={item.name}
+                    price={item.price}
+                    sale={item.discount}
+                    rating={item.rating}
+                  />
+                </NavLink>
+              ))
+          : dresses.map((item) => (
+              <NavLink
                 key={item.id}
-                img={item.images[0].url}
-                title={item.name}
-                price={item.price}
-                sale={item.discount}
-                rating={item.rating}
-              />
-            </NavLink>
-          ))}
+                to={`/${productType}/${item.id}`}
+                className={styles.link}
+                data-test-id={`clothes-card-${productType}`}
+              >
+                <GridItem
+                  key={item.id}
+                  img={item.images[0].url}
+                  title={item.name}
+                  price={item.price}
+                  sale={item.discount}
+                  rating={item.rating}
+                />
+              </NavLink>
+            ))}
       </div>
     </div>
   );
