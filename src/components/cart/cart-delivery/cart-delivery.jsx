dd29ba645b7c0bfaa5e2864430829c0abb4081e2
Gaurrus <listenbulletformyvalentine@gmail.com>
2022-03-06 14:37:29 +0300

    немного за дизайн

--- src/components/cart/cart-delivery/cart-delivery.jsx
+++ src/components/cart/cart-delivery/cart-delivery.jsx
@@ -1,3 +1,20 @@
+import classNames from 'classnames';
 import styles from './cart-delivery.module.scss';
 
-export const CartDelivery = () => <div className={styles.wrapper}>Доставка</div>;
+export const CartDelivery = () => (
+  <div className={styles.wrapper}>
+    Доставка
+    <div className={styles.cardFooter}>
+      <div className={styles.totalPrice}>
+        <span className={styles.totalText}>Total</span>
+        <span className={styles.totalPrice}>$100.500</span>
+      </div>
+      <button type='button' className={classNames(styles.further, styles.button)}>
+        Further
+      </button>
+      <button type='button' className={classNames(styles.viewCart, styles.button)}>
+        View Cart
+      </button>
+    </div>
+  </div>
+);
--- src/components/cart/cart-payment/cart-payment.jsx
+++ src/components/cart/cart-payment/cart-payment.jsx
@@ -1,3 +1,20 @@
+import classNames from 'classnames';
 import styles from './cart-payment.module.scss';
 
-export const CartPayment = () => <div className={styles.wrapper}>продукты</div>;
+export const CartPayment = () => (
+  <div className={styles.wrapper}>
+    продукты
+    <div className={styles.cardFooter}>
+      <div className={styles.totalPrice}>
+        <span className={styles.totalText}>Total</span>
+        <span className={styles.totalPrice}>$100.500</span>
+      </div>
+      <button type='button' className={classNames(styles.further, styles.button)}>
+        Further
+      </button>
+      <button type='button' className={classNames(styles.viewCart, styles.button)}>
+        View Cart
+      </button>
+    </div>
+  </div>
+);
