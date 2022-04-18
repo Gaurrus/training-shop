e30294e2fdedab679d869d6221e8bf5e7205fe80
Gaurrus <listenbulletformyvalentine@gmail.com>
2022-04-18 16:43:30 +0300

    fix: адрес отправки формы и кнопки further

--- src/components/utils/validate-form.js
+++ src/components/utils/validate-form.js
@@ -1,7 +1,7 @@
 export const validate = (values) => {
   const errors = {};
   if (!values.mail) {
-    errors.mail = 'required field';
+    errors.mail = 'Поле должно быть заполнено';
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.mail)) {
     errors.mail = 'E-mail format error';
   }
@@ -11,10 +11,10 @@ export const validate = (values) => {
 export const validateReview = (values) => {
   const errors = {};
   if (!values.review) {
-    errors.review = 'required field';
+    errors.review = 'Поле должно быть заполнено';
   }
   if (!values.username) {
-    errors.username = 'required field';
+    errors.username = 'Поле должно быть заполнено';
   }
   return errors;
 };
@@ -28,45 +28,45 @@ const toodayYear = date.getFullYear() - 2000;
 export const validateCart = (values) => {
   const errors = {};
   if (!values.phone || values.phone === '+375(__)_______') {
-    errors.phone = 'required field';
+    errors.phone = 'Поле должно быть заполнено';
   } else if (!/^(\+?375)(\()(33|44|29|25|)(\))(\d*)/i.test(values.phone)) {
-    errors.phone = 'phone format error';
+    errors.phone = 'Ошибка формата номера';
   }
   if (!values.email) {
-    errors.email = 'required field';
+    errors.email = 'Поле должно быть заполнено';
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-    errors.email = 'E-mail format error';
+    errors.email = 'Ошибка формата e-mail';
   }
   if (!values.country) {
-    errors.country = 'required field';
+    errors.country = 'Поле должно быть заполнено';
   }
   if (!values.cashEmail) {
-    errors.cashEmail = 'required field';
+    errors.cashEmail = 'Поле должно быть заполнено';
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.cashEmail)) {
-    errors.cashEmail = 'E-mail format error';
+    errors.cashEmail = 'Ошибка формата e-mail';
   }
   if (!values.city) {
-    errors.city = 'required field';
+    errors.city = 'Поле должно быть заполнено';
   }
   if (!values.street) {
-    errors.street = 'required field';
+    errors.street = 'Поле должно быть заполнено';
   }
   if (!values.house) {
-    errors.house = 'required field';
+    errors.house = 'Поле должно быть заполнено';
   }
   if (!values.postcode || values.postcode === 'BY ______') {
-    errors.postcode = 'required field';
+    errors.postcode = 'Поле должно быть заполнено';
   }
   if (!values.storeAddress) {
-    errors.storeAddress = 'required field';
+    errors.storeAddress = 'Поле должно быть заполнено';
   }
   if (!values.card || values.card === '____-____-____-____') {
-    errors.card = 'required field';
+    errors.card = 'Поле должно быть заполнено';
   } else if (!/(\d){4}(-)(\d){4}(-)(\d){4}(-)(\d){4}/i.test(values.card)) {
-    errors.card = 'card number incorrect';
+    errors.card = 'некорректный номер карты';
   }
   if (!values.cardDate || values.cardDate === '__/__') {
-    errors.cardDate = 'required field';
+    errors.cardDate = 'Поле должно быть заполнено';
   } else if (
     (+values.cardDate.slice(-2) === +toodayYear &&
       +values.cardDate.slice(0, 2) >= 1 &&
@@ -74,16 +74,16 @@ export const validateCart = (values) => {
     +values.cardDate.slice(0, 2) > 12 ||
     +values.cardDate.slice(0, 2) <= 0
   ) {
-    errors.cardDate = 'month error';
+    errors.cardDate = 'ошибка месяца';
   } else if (+values.cardDate.slice(-2) < +toodayYear) {
     console.log(+values.cardDate.slice(0, 2));
-    errors.cardDate = 'year error';
+    errors.cardDate = 'ошибка года';
   }
   if (!values.cardCVV || values.cardCVV === '___') {
-    errors.cardCVV = 'required field';
+    errors.cardCVV = 'Поле должно быть заполнено';
   }
   if (!values.agreenment) {
-    errors.agreenment = 'required field';
+    errors.agreenment = 'Поле должно быть заполнено';
   }
   return errors;
 };
